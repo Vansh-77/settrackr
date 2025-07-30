48213b83fa5e36008e056d1355898bcfa34a1d08
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-07-30 21:02:56 +0530

    auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,51 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import { User } from "../models/user";
 
+function generatetoken(id){
+    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'30d'})
+}
 
 export const registor = async (req ,res) => {
-    
+    try {
+        const {email , username , password , gender , age}= req.body;
+        if(!email || !username || !password || !gender || !age){
+            return res.status(400).json({message:"Please fill all fields"})
+        }
+        if(password.length<8){
+            return res.status(400).json({message:"Password must be at least 8 characters"})
+        }
+        const existingEmail = await User.findOne({ email });
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+        const user = new  User({
+            email,
+            username,
+            password,
+            gender,
+            age
+        })
+        await user.save();
+        const token = generatetoken(user._id);
+        
+        res.status(201).json({
+            token,
+            user:{
+                email,
+                username,
+                gender,
+                age
+            }
+        })  
+    } catch (error) {
+        console.log("registration error",error);
+        res.status(500).json({message:"Internal server error"});  
+    }
 };
 export const login = async (req ,res) => {
     
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -14,7 +15,21 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    gender:{
+        type:String,
+    },
+    age:{
+        type:Number,
+    }
      
 },{timestamps:true});
+
+userSchema.pre("save",async function (next) {
+    if(this.isModified("password")){
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+    }
+    next();    
+})
 
 export const User = new mongoose.model("User",userSchema);
