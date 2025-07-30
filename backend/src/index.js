cced86fa0990d7fe2eba6daadea40700bbc52c07
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-07-30 21:59:51 +0530

    imported routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import "dotenv/config"
 import AuthRoutes from "./routes/auth.routes.js"
 import RoutineRoutes from "./routes/routine.routes.js"
+import WorkoutRoutes from "./routes/workout.routes.js"
 import { ConnectDB } from "./config/db.js";
 
 const app = express();
