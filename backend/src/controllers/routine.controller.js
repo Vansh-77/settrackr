a334e4b4ac2f5c3555d2a9b589b56fb5c0dfec2f
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-08-01 16:37:16 +0530

    getRoutines

--- backend/src/controllers/routine.controller.js
+++ backend/src/controllers/routine.controller.js
@@ -9,21 +9,39 @@ export const createRoutine = async (req, res) => {
         const routine = new Routine({
             userId:req.user._id,
             ...routineData
-        })
+        });
         await routine.save();
         res.status(201).json({
             message:"routine created successfully",
-            routine
+            routine,
         });
 
     } catch (error) {
         console.log(error);
         res.status(500).json({message:"Internal server error"});
     }
-
 }
 export const getRoutines = async (req, res) => {
-
+    try {
+        const page = req.page || 1;
+        const limit = req.limit || 5;
+        const skip = (page-1)*limit;
+        const count = await Routine.countDocuments({userId:req.user._id});
+        const totalpages = Math.ceil(count/limit);
+        const routines = await Routine.find({userId:res.user._id}).skip(skip).limit(limit).sort({createdAt:-1});
+        if(!routines || routines.length===0){
+            return res.status(404).json({message:"No routine found"});
+        }
+        res.status(200).json({
+            page,
+            count,
+            totalpages,
+            routines
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 export const updateRoutine = async (req, res) => {
 
